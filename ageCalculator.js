ffa255fea7c003210da8c57685135de558d7acce
luigifort <126508080+luigifort@users.noreply.github.com>
2024-01-15 20:13:09 +0000

    Update 4 files

---
+++ ageCalculator.js
@@ -0,0 +1,7 @@
+const date = new Date();
+const today = date.getDate();
+console.log(today);
+const month = date.getMonth();
+console.log(month);
+const year = date.getFullYear();
+console.log(year);
